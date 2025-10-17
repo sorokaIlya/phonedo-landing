0517f1c4cce7329c4321105b260ab35d3806de6d
revol <revolut_mozart@icloud.com>
2025-10-17 18:33:06 +0300

    add styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import './style.css'
 
+const dialog = document.getElementById('dialogDemo');
+const dialog2 = document.getElementById('dialogThanks');
+document.querySelectorAll('[data-demo]').forEach((item) => {
+    item.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        if (dialog && typeof dialog.showModal === 'function') {
+            dialog.showModal();
+        } else {
+            console.warn('Dialog element not found or not a <dialog>.');
+        }
+    })
+});
+document.querySelectorAll('[data-close]').forEach((item) => {
+    item.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        dialog.close();
+    })
+});
+
+
 // const burger = document.querySelector('.footer__burger');
 // const nav = document.querySelector('.footer__nav');
 //
--- vite-config.js
+++ vite-config.js
@@ -1,17 +1,18 @@
-import {defineConfig} from "vite";
-import {resolve} from "path";
+import {defineConfig} from 'vite'
+import {resolve} from 'path';
 
 export default defineConfig({
     base: '/phonedo-landing/',
     build: {
         rollupOptions: {
             input: {
-                home: resolve(__dirname, 'index.html'),
-                price: resolve(__dirname, 'price.html'),
-                contact: resolve(__dirname, 'contact.html'),
-                features: resolve(__dirname, 'features.html'),
-                agents: resolve(__dirname, 'agents.html'),
-                working: resolve(__dirname, 'how-work.html'),
+                main: resolve(__dirname, 'index.html'),
+                price: resolve(__dirname, 'pages/pricing.html'),
+                about: resolve(__dirname, 'pages/about.html'),
+                contact: resolve(__dirname, 'pages/contact.html'),
+                features: resolve(__dirname, 'pages/features.html'),
+                agents: resolve(__dirname, 'pages/agents.html'),
+                working: resolve(__dirname, 'pages/how-work.html'),
             },
         },
     },
